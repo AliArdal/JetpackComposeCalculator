503b0c9fbdc95cddb00e0ad50b11b5b602b7f5ba
Ali Ardal <ali_ardal@outlook.com>
2024-08-25 16:04:29 +0300

    Odev5

---
+++ Odev5/app/src/main/java/com/example/odev5/CalculatorViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.odev5
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.mozilla.javascript.Context
+import org.mozilla.javascript.Scriptable
+
+class CalculatorViewModel : ViewModel() {
+
+    private val _equationText = MutableLiveData("")
+    val equationText: LiveData<String> = _equationText
+
+    private val _resultText = MutableLiveData("0")
+    val resultText: LiveData<String> = _resultText
+
+    fun onButtonClick(btn: String) {
+        Log.i("Clicked Button", btn)
+
+        _equationText.value?.let {
+            when (btn) {
+                "AC" -> {
+                    _equationText.value = ""
+                    _resultText.value = "0"
+                }
+
+                "+" -> {
+                    _equationText.value = it + "+"
+                }
+
+                else -> {
+                    _equationText.value = it + btn
+                    try {
+                        _resultText.value = calculateResult(_equationText.value.toString())
+                    } catch (_: Exception) {
+                        _resultText.value = "ERROR"
+                    }
+                }
+            }
+        }
+    }
+
+    private fun calculateResult(equation: String): String {
+        val context: Context = Context.enter()
+        context.optimizationLevel = -1
+        val scriptable: Scriptable = context.initStandardObjects()
+        var finalResult = context.evaluateString(scriptable, equation, "Javascript", 1, null).toString()
+        if (finalResult.endsWith(".0")) {
+            finalResult = finalResult.replace(".0", "")
+        }
+        return finalResult
+    }
+}
